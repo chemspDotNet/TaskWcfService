58c3e2265822dfd70626e7bc96137e316e14c2d7
Sunil patel <chemsp@outlook.com>
2021-03-18 16:33:28 -0400

    Add project files.

---
+++ Models/Quote.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace TaskWcfService.Models
+{
+    [DataContract]
+    public class Quote
+    {
+        [DataMember]
+        public int QuoteID { get; set; }
+        [DataMember]
+        public string QuoteName { get; set; }
+        [DataMember]
+        public string QuoteType { get; set; }
+
+        public Quote()
+        {
+
+        }
+
+        public Quote(int quoteID, string quoteName, string quoteType )
+        {
+            this.QuoteID = quoteID;
+            this.QuoteName = quoteName;
+            this.QuoteType = quoteType;
+                
+
+        }
+    }
+}
